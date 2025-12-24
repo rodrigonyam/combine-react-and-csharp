32e00637ad541378332e773a2eb1d2120c24792d
Rodrigue Nyamwoga <rodrigonyam@gmail.com>
2025-12-24 00:46:26 -0500

    dependencies

--- backend/obj/Debug/net10.0/ProjectManagement.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/ProjectManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2745ee3642ca81ef205f912b0a4f691f09807ade")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
